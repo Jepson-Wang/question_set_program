5a7c2e6f3ef705fa34d4fbe41b6ebb6eaee944fe
PING <3395637566@qq.com>
2026-03-08 12:38:42 +0800

    主程序

---
+++ backend/main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from api.agent_api import agent_router
+from api.login_api import login_api
+
+app = FastAPI(
+    debug=True,
+    title="学生学情分析系统",
+    openapi_url="/api"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(agent_router)
+app.include_router(login_api)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
