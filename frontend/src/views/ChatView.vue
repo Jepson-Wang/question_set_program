8ef841b564a0bec5840eb8d028251d72a69c0525
mdd <mddnbb66@gmail.com>
2026-03-10 15:09:59 +0800

    初始化前端页面骨架

---
+++ frontend/src/views/ChatView.vue
@@ -0,0 +1,12 @@
+
+<template>
+        <h1>聊天页</h1>
+</template>
+    
+<script setup lang='ts'>
+    
+</script>
+    
+<style>
+    
+</style>
---
+++ frontend/src/views/RegisterView.vue
@@ -0,0 +1,11 @@
+<template>
+        <h1>注册页</h1>
+</template>
+    
+<script setup lang='ts'>
+    
+</script>
+    
+<style>
+    
+</style>
