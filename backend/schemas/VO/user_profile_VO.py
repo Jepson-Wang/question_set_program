7e2d2476d6a4b22d19d572acf6b77e60a6dcf31a
Jepson Wang <3395637566@qq.com>
2026-03-17 22:10:20 +0800

    对所传字段进行封装，成为一个个对象

---
+++ backend/schemas/VO/user_profile_VO.py
@@ -0,0 +1,8 @@
+from pydantic import BaseModel,Field
+
+class UserProfileVO(BaseModel):
+    user_id: str = Field(description='用户ID', example=1, gt=0)
+    grade: Field(description='年级', example='七年级')
+    subject: Field(description='主修学科', example='数学')
+    weak_points: Field(description='薄弱知识点', example={'数学': '导数'})
+    preferences: Field(description='长期偏好', example={'学习方式': '视频'})
