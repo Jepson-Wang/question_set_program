3b6146a8a9a8b8c103998de4a25fe34076d82f85
Jepson Wang <3395637566@qq.com>
2026-03-24 15:48:48 +0800

    小改动

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter
 from backend.api.manage_api import manage_api
 from backend.api.user_api import user_api
 
-api=APIRouter()
-api.include_router(user_api)
-api.include_router(manage_api)
+api_manager=APIRouter(prefix='/api')
+api_manager.include_router(user_api)
+api_manager.include_router(manage_api)
 
